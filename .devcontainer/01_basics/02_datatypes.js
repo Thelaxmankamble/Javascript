883650b70030da5d569cd904c1c3047fa949e598
Laxman kamble <164211652+Thelaxmankamble@users.noreply.github.com>
2025-04-07 18:48:03 +0000

    data types

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict;" // this will treat all JS the code as new version 
+
+//alert(3+3); this will give error as we are writing it in nodejs not in browser
+
+// data types
+// numbers=> 2 to power 53 is a range for numbers
+//boolean=> true/false
+//bigInt=> used for huge numbers
+//string=> ""
+//null=> standalone value for eg suppose i want give the temperature but i dont have it for now so will give the variable value as null not '0' because its a value .
+//undefined=> value not assigned yet. for eg : let state; state has not been assigned any value.
+
+console.log(typeof undefined);// undefined is a type itself
+console.log(typeof null);//it of type object
+
---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 123456;
+let accountEmail = "laxman@google.com"
+var accountPassword = "878787"
+accountCity = "Mumbai"
+
+// accountId = 323232
+ accountEmail = "lax@google.com";
+
+// var tester = "hey hi";
+console.log(accountEmail);
+
+
+ 
