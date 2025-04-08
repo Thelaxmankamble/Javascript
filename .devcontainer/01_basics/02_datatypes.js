ad8f487ad60c24a8fa2f6421670bb4dddd7585ee
Laxman kamble <164211652+Thelaxmankamble@users.noreply.github.com>
2025-04-08 17:50:43 +0000

    conversion of data types

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -7,9 +7,9 @@
 //boolean=> true/false
 //bigInt=> used for huge numbers
 //string=> ""
-//null=> standalone value for eg suppose i want give the temperature but i dont have it for now so will give the variable value as null not '0' because its a value .
+//null=> standalone value for eg suppose i want to give the temperature but i dont have it for now so i will give the variable value as null not '0' because zero is a value .
 //undefined=> value not assigned yet. for eg : let state; state has not been assigned any value.
 
 console.log(typeof undefined);// undefined is a type itself
-console.log(typeof null);//it of type object
+console.log(typeof null);//its of type object
 
---
+++ .devcontainer/01_basics/conversionOp.js
@@ -0,0 +1,21 @@
+// let score = "abc33";
+
+console.log(typeof score);
+
+// let isloggedIn = 0;
+// let BooleanIsloggedIn = Boolean(isloggedIn);
+
+// console.log(BooleanIsloggedIn);
+
+// in boolean conversion 1 => gives true value and 0=> gives false value
+// for empty string "" => gives false value
+// for string with value in it , it gives value as true
+
+// for string 
+let someNumber = 17;
+
+let StringsomeNumber = String(someNumber);
+console.log(StringsomeNumber);
+
+
+
