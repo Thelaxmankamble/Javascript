c788c9b606609ceb9d34516e6f5f1ef734b2469e
Laxman kamble <164211652+Thelaxmankamble@users.noreply.github.com>
2025-04-12 12:50:17 +0000

    maths.nums done

---
+++ .devcontainer/01_basics/numbers.js
@@ -0,0 +1,22 @@
+const score = 170904;
+console.log(score);
+
+const Balance = Number(100);
+console.log(Balance);
+
+let score2 = 1000000;
+console.log(score2.toLocaleString('en-IN'));
+
+// maths.....
+
+console.log(Math.abs(-5)); // gives absolute value like if it is negative then gives +ve value and for +ve it is +ve only.
+console.log(Math.round(9.4)); // gives round off value just like it is in maths.
+console.log(Math.ceil(9.8));//ceil gives top value even if it is in lower integer
+console.log(Math.floor(9.8));//gives bottom value integer even if it is top integer
+console.log(Math.min(2, 4, 5, 6, 1, 9));// gives minimum value from the object
+console.log(Math.max(2, 4, 5, 6, 1, 9));// gives maximum value from the object
+
+console.log(Math.random)// gives us value b/w 0 to 1
+
+
+
