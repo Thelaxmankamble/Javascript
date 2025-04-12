7763c1d4b416a605cb4137fb07e0187661f44d4b
Laxman kamble <164211652+Thelaxmankamble@users.noreply.github.com>
2025-04-12 12:51:32 +0000

    datatypes

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -1,14 +1,20 @@
-"use strict;" // this will treat all JS the code as new version 
+"use strict;" // this will treat all the JS code as new version 
 
 //alert(3+3); this will give error as we are writing it in nodejs not in browser
 
-// data types
+
+// these below mentioned are primitive data types
+// data types(call by value)
 // numbers=> 2 to power 53 is a range for numbers
 //boolean=> true/false
 //bigInt=> used for huge numbers
 //string=> ""
 //null=> standalone value for eg suppose i want to give the temperature but i dont have it for now so i will give the variable value as null not '0' because zero is a value .
 //undefined=> value not assigned yet. for eg : let state; state has not been assigned any value.
+
+
+// non-primitive(call by reference) data types are:-
+// Arrays, functions, objects
 
 console.log(typeof undefined);// undefined is a type itself
 console.log(typeof null);//its of type object
